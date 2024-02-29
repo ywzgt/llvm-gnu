bae37859e4157043b324586e59471340f67d2df5
ywzgt <92680591+ywzgt@users.noreply.github.com>
2024-03-01 07:38:53 +0800

    Update envars.sh

--- envars.sh
+++ envars.sh
@@ -7,7 +7,7 @@ case $(uname -m) in
 		;;
 esac
 
-export CFLAGS="$CFLAGS -mtune=haswell -O2 -pipe -fno-plt -fPIC -ffunction-sections -fdata-sections"
+export CFLAGS="-mtune=haswell -O2 -pipe -fno-plt -fPIC -ffunction-sections -fdata-sections"
 export CXXFLAGS="$CFLAGS -Wp,-D_GLIBCXX_ASSERTIONS"
 export CPPFLAGS="-D_FORTIFY_SOURCE=2 -DNDEBUG"
 export LDFLAGS="-Wl,-O2,--sort-common,--as-needed,-z,relro,-z,now,--gc-sections"
