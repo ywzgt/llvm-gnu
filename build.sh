552204f7d81e72e0d1162075598e02a5410349b8
ywzgt <92680591+ywzgt@users.noreply.github.com>
2024-03-04 18:59:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,11 +12,7 @@ SRC=(
 	${URL}/releases/download/llvmorg-${VERSION}/llvm-${VERSION}.src.tar.xz
 	${URL}/releases/download/llvmorg-${VERSION}/cmake-${VERSION}.src.tar.xz
 	${URL}/releases/download/llvmorg-${VERSION}/third-party-${VERSION}.src.tar.xz
-
 	${URL}/releases/download/llvmorg-${VERSION}/clang-${VERSION}.src.tar.xz
-	${URL}/releases/download/llvmorg-${VERSION}/lld-${VERSION}.src.tar.xz
-	${URL}/releases/download/llvmorg-${VERSION}/libunwind-${VERSION}.src.tar.xz
-
 	https://www.linuxfromscratch.org/patches/blfs/svn/clang-17-enable_default_ssp-1.patch
 )
 
@@ -28,14 +24,13 @@ for arg in $@; do
 	esac
 done
 
-rm -rf *-${VERSION}.src \
-    {cmake,third-party,libunwind}
+rm -rf *-${VERSION}.src cmake third-party
 for i in ${SRC[@]}; do
 	wget -nv -c $i
 	if ! [[ $i =~ BLFS ]]; then
 		f=$(basename $i)
 		if [[ $f = *.src.tar.xz && ! -d ${f%.tar.xz} ]]
-        then
+        	then
 			echo "Extracting $f..."
 			tar xf $f &
 		fi
@@ -43,35 +38,24 @@ for i in ${SRC[@]}; do
 done
 
 while pidof -q tar; do sleep 0.1; done
-for i in cmake third-party libunwind
+for i in cmake third-party
 do
     ln -s $i{-${VERSION}.src,}
 done
 
 cd llvm-${VERSION}.src
 mv ../clang-${VERSION}.src tools/clang
-mv ../lld-${VERSION}.src tools/lld
 grep -rl '#!.*python' | xargs sed -i '1s/python$/python3/'
 
-if [[ $ELIBC != musl ]]; then
+if [[ $ELIBC = gnu ]]; then
 	patch -Np2 -d tools/clang <../clang-17-enable_default_ssp-1.patch
-else
-	_args=(-DCOMPILER_RT_BUILD_GWP_ASAN=OFF)
 fi
 
 src_config() {
-	local _flags=(
-		-DCLANG_DEFAULT_LINKER=lld
-		-DCLANG_DEFAULT_OPENMP_RUNTIME=libgomp
-		-DCLANG_DEFAULT_OBJCOPY=llvm-objcopy
-	)
-
 	if command -v clang{,++} > /dev/null; then
-		CC=clang CXX=clang++ "$@" \
-		-DLLVM_USE_LINKER=lld "${_flags[@]}"
+		CC=clang CXX=clang++ "$@"
 	else
-		CC=gcc CXX=g++ "$@" \
-		-DLLVM_USE_LINKER=gold
+		CC=gcc CXX=g++ "$@"
 	fi
 }
 
@@ -86,9 +70,11 @@ cmake -DCMAKE_INSTALL_PREFIX=/usr           \
       -DLLVM_BINUTILS_INCDIR=/usr/include   \
       -DLLVM_INCLUDE_BENCHMARKS=OFF         \
       -DCLANG_DEFAULT_PIE_ON_LINUX=ON       \
+      -DLLVM_USE_LINKER=gold \
       -DLLVM_BUILD_TESTS=OFF \
       -DLLVM_INCLUDE_TESTS=OFF \
       -DLLVM_HOST_TRIPLE=$(gcc -dumpmachine) \
+      -DCLANG_DEFAULT_OPENMP_RUNTIME=libgomp \
       -DCLANG_CONFIG_FILE_SYSTEM_DIR=/usr/lib/clang \
       -Wno-dev -G Ninja "${_args[@]}" -B build
 
@@ -100,7 +86,8 @@ DESTDIR=$PKG ninja -C build install &> /dev/null
 # https://packages.gentoo.org/packages/sys-devel/clang-common
 cat > $PKG/usr/lib/clang/clang.cfg <<-EOF
 	# It is used to control the default runtimes using by clang.
-
+ 
+	-fuse-ld=gold
 EOF
 
 ln -s clang.cfg "$PKG/usr/lib/clang/clang++.cfg"
