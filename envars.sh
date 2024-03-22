c98f11d6b95ed778735110bc3ab09a0a45f8bff5
ywzgt <ywzgt@users.noreply.github.com>
2024-03-22 15:56:11 +0800

    2

--- envars.sh
+++ envars.sh
@@ -9,7 +9,7 @@ esac
 
 export CFLAGS="$CFLAGS -mtune=haswell -O2 -pipe -fno-plt -fPIC -fstack-clash-protection -ffunction-sections -fdata-sections"
 export CXXFLAGS="$CFLAGS -Wp,-D_GLIBCXX_ASSERTIONS"
-export CPPFLAGS="-D_FORTIFY_SOURCE=3 -DNDEBUG"
+export CPPFLAGS="-D_FORTIFY_SOURCE=2 -DNDEBUG"
 export LDFLAGS="-Wl,-O2,--sort-common,--as-needed,-z,relro,-z,now,--gc-sections"
 export MAKEFLAGS="-j$(nproc)"
 export NINJAJOBS="$(nproc)"
