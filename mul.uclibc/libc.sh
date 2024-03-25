7a35ad007d945850f561f44e2f242fc82881abb0
ywzgt <ywzgt@users.noreply.github.com>
2024-03-26 06:02:49 +0800

    multilib.20

--- mul.uclibc/libc.sh
+++ mul.uclibc/libc.sh
@@ -8,24 +8,23 @@ wget -nv https://downloads.uclibc-ng.org/releases/$PV/$SRC-$PV.tar.xz
 tar xf $SRC-$PV.tar.xz
 cp .config $SRC-$PV
 cd $SRC-$PV
-patch -Np1 -i ../uClibc-add-getentropy-from-musl.patch
-patch -Np1 -i ../uClibc-ng-merge_usr.patch
-patch -Np1 -i ../ldconfig-no-warn-default.patch
+
+for i in ../00*.patch; do
+	echo "PATCH: ${i##*/}"
+	patch -Np1 -i $i
+done
 
 sed -i.old "/^UCLIBC_EXTRA_CFLAGS/s/=.*/=\"$CFLAGS $LDFLAGS\"/" .config
 timeout 10 make silentoldconfig
 diff -u --color .config{.old,} || true
 
 sed -i '/\(libintl\|crypt\).h$/d' Makefile.in
-sed -i.ori '/UCLIBC_RUNTIME_PREFIX/s/lib/&32/g' \
-	ldso/ldso/dl-elf.c \
-	utils/ld{d,config}.c
-
 rm -fv include/{crypt,libintl}.h
 make
 make utils
+ar r libssp_noshared.a libc/sysdeps/linux/common/ssp-local.os
 
 make install DESTDIR=$PWD/p
 make install_utils DESTDIR=$PWD/p
-ln -sv uclibc_nonshared.a p/usr/lib32/libssp_nonshared.a
+cp libssp_nonshared.a p/usr/lib32/
 cp -a p /build
