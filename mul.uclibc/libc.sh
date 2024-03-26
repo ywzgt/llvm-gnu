03080f2f2a14d41a97b4de6f277a49812dda791b
ywzgt <ywzgt@users.noreply.github.com>
2024-03-26 15:21:23 +0800

    multilib.21

--- mul.uclibc/libc.sh
+++ mul.uclibc/libc.sh
@@ -4,11 +4,18 @@ SRC="uClibc-ng"
 PV="1.0.46"
 
 . ../envars.sh
+[[ $1 -eq 64 ]] || rm -fv 003-x86_64-multilib.patch
 wget -nv https://downloads.uclibc-ng.org/releases/$PV/$SRC-$PV.tar.xz
 tar xf $SRC-$PV.tar.xz
-cp .config $SRC-$PV
-cd $SRC-$PV
 
+if [[ $1 -eq 64 ]]; then
+	rm -fv 003-x86_64-multilib.patch
+	cp config-x86_64 $SRC-$PV/.config
+else
+	cp .config $SRC-$PV
+fi
+
+cd $SRC-$PV
 for i in ../00*.patch; do
 	echo "PATCH: ${i##*/}"
 	patch -Np1 -i $i
@@ -22,9 +29,15 @@ sed -i '/\(libintl\|crypt\).h$/d' Makefile.in
 rm -fv include/{crypt,libintl}.h
 make
 make utils
-ar r libssp_noshared.a libc/sysdeps/linux/common/ssp-local.os
+ar r libssp_nonshared.a libc/sysdeps/linux/common/ssp-local.os
 
-make install DESTDIR=$PWD/p
-make install_utils DESTDIR=$PWD/p
-cp libssp_nonshared.a p/usr/lib32/
-cp -a p /build
+if [[ $1 -eq 64 ]]; then
+	make install
+	make install_utils
+	cp libssp_nonshared /usr/lib
+else
+	make install DESTDIR=$PWD/p
+	make install_utils DESTDIR=$PWD/p
+	cp libssp_nonshared.a p/usr/lib32/
+	cp -a p /build
+fi
